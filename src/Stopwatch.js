4426388a4c1e2704341f9f1418b9d8688f012786
Elena Santana <fanelenan7@gmail.com>
2017-03-24 16:08:58 -0400

    start button works

--- src/Stopwatch.js
+++ src/Stopwatch.js
@@ -2,13 +2,36 @@ import React, { Component } from 'react';
 import "./Stopwatch.css"
 
 class Stopwatch extends Component {
+  constructor() {
+    super()
+    this.state = {
+      time: 0
+    }
+  }
+  resetButton(){
+    clearInterval(this.state.time)
+    this.state.time = 0
+  }
+  startButton(){
+    setInterval(() => {
+      this.setState({
+        time: this.state.time + 1,
+      })
+    }, 1000)
+  }
+  // startButton(){
+  //   this.setState(
+  //      time: setInterval(() => {this.state.time + 1}, 1000)
+  //   )
+  // }
+
   render() {
     return (
       <div className="stopwatch">
-        <h1>0</h1>
+        <h1>{this.state.time}</h1>
         <div className="controls">
-          <button>Reset</button>
-          <button>Start</button>
+          <button onClick={()=>this.resetButton()}>Reset</button>
+          <button onClick={()=> this.startButton()}>Start</button>
           <button>Pause</button>
         </div>
       </div>
